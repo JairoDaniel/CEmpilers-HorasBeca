207f141903e125ef8d7d1d97f6d319e4b20d64c9
Jairo <jairo.orca23@gmail.com>
2018-05-27 03:34:20 -0600

    Actualizacion funciones de la comisión

---
+++ HorasBeca/Comision/Controllers/evaluarController.cs
@@ -0,0 +1,79 @@
+﻿using Comision.Constants;
+using Comision.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Comision.Controllers
+{
+    [RoutePrefix("comision")]
+    public class evaluarController : ApiController
+    {
+        [Route("aprobarSolicitud")]
+        [HttpPost]
+        public void aprobar(evaluar solicitud)
+        {
+            using (SqlConnection connection = DBConnection.getConnection())
+            {
+
+                SqlCommand command = new SqlCommand("dbo.aprobar", connection);
+                SqlCommand commandHora = new SqlCommand("dbo.asignar_horas", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                commandHora.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@id_solicitud", SqlDbType.Int).Value = Convert.ToInt32(solicitud.id_solicitud);
+                command.Parameters.AddWithValue("@horas", SqlDbType.Int).Value = Convert.ToInt32(solicitud.horas);
+                command.Parameters.AddWithValue("@responsable", SqlDbType.VarChar).Value = solicitud.responsable;
+
+
+
+                commandHora.Parameters.AddWithValue("@carnet", SqlDbType.Int).Value = Convert.ToInt32(solicitud.carnet);
+                commandHora.Parameters.AddWithValue("@horas", SqlDbType.Int).Value = Convert.ToInt32(solicitud.horas);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    commandHora.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally { connection.Close(); }
+            }
+        }
+
+        [Route("rechazarSolicitud")]
+        [HttpPost]
+        public void rechazar(evaluar solicitud)
+        {
+            using (SqlConnection connection = DBConnection.getConnection())
+            {
+
+                SqlCommand command = new SqlCommand("dbo.rechazar", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                command.Parameters.AddWithValue("@id_solicitud", SqlDbType.Int).Value = Convert.ToInt32(solicitud.id_solicitud);
+                command.Parameters.AddWithValue("@observacion", SqlDbType.VarChar).Value = solicitud.observacion;
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+                finally { connection.Close(); }
+            }
+        }
+
+    }
+}
--- HorasBeca/Comision/Models/evaluar.cs
+++ HorasBeca/Comision/Models/evaluar.cs
@@ -5,10 +5,12 @@ using System.Web;
 
 namespace Comision.Models
 {
-    public class aprobar
+    public class evaluar
     {
         public int id_solicitud { get; set; }
+        public int carnet { get; set; }
         public int horas { get; set; }
         public string responsable { get; set; }
+        public string observacion { get; set; }
     }
 }
