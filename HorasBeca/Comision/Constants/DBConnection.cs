37681d64dbdfc77e18e5bd40fdbc909899d93c1e
Jairo <jairo.orca23@gmail.com>
2018-06-15 00:31:22 -0600

    Actualizacion de los connections string

--- HorasBeca/Comision/Constants/DBConnection.cs
+++ HorasBeca/Comision/Constants/DBConnection.cs
@@ -10,7 +10,15 @@ namespace Comision.Constants
     {
         public static SqlConnection getConnection()
         {
-            SqlConnection connection = new SqlConnection("Data Source = DESKTOP-059I8H7\\SQLSERVERHB; Initial Catalog = HorasBecaDB; Integrated Security = true;");
+
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+            builder.DataSource = "DESKTOP-059I8H7\\SQLSERVERHB";
+            builder.InitialCatalog = "HorasBecaDB";
+            builder.UserID = "horasBecaSQL";
+            builder.Password = "horasBecaSQL";
+
+            SqlConnection connection = new SqlConnection(builder.ConnectionString);
             return connection;
         }
     }
