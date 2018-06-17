d4b9f4c0fc71763f249dc3af92b1b2a80d38fece
Emily Sancho <emilysancho@gmail.com>
2018-06-17 05:30:34 -0600

    Todo bien pero sin pruebas ...

---
+++ HorasBeca/DB/PopulacionDB.sql
@@ -0,0 +1,95 @@
+--USE HorasBecaDB
+execute insertar_solicitud 1,206920,20141,'Anderson','Taylor','Cordero',89490,'estudiante','taylor@gmail.com',55.12,65,'si','200-01-001',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206921,20142,'Geovanny','Espinoza','Quieros',89491,'asistente','geovanny@gmail.com',76.065,70.21,'si','200-01-002',NULL,NULL,NULL,'enviada','pendiente','si',50,'especial','Ciencias de lenguaje'
+execute insertar_solicitud 1,206922,20143,'Juan Agustin','Salazar','Quesada',89492,'tutor','juan@gmail.com',86.820,90.14,'si','200-01-003',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206923,20144,'Wilson Armando','Lopez','Rubí',89493,'especial','wilson@gmail.com',65.02,46.945,'no','200-01-004',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206924,20145,'Roy','Diaz','Zuñiga',89494,'estudiante','roy@gmail.com',70.252,70.0789,'si','200-01-005',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206925,20146,'Javier','Salazar','Quesada',89495,'asistente','javier@gmail.com',98.26,84.156,'no','200-01-006',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206926,20147,'Fabian','Esquivel','Arias',89496,'tutor','fabian@gmail.com',69.520,70.0789,'si','200-01-007',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206927,20148,'Geiner','Carrillo','Carevaca',89497,'especial','alejandro@gmail.com',67.214,68.23,'si','200-01-008',NULL,NULL,NULL,'enviada','pendiente','si',20,'tutoria','Escuela de matematicas'
+execute insertar_solicitud 1,206928,20149,'Alejandro','Jimenez','Cordero',89498,'estudiante','alejandro@gmail.com',67.156,60,'si','200-01-009',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206929,201410,'Andy','Carrillo','Rosales',89499,'asistente','andy@gmail.com',70.2,63.20,'no','200-01-010',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,2069210,201411,'Steven','Carrillo','Rosales',894910,'tutor','steven@gmail.com',67.5,80.32,'si','200-01-011',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,2069211,201412,'Nelson','Ramirez','Castro',894911,'especial','nelson@gmail.com',73.2,70.78,'no','200-01-012',NULL,NULL,NULL,'enviada','pendiente','si',15,'asistente','Escuela de computacion'
+execute insertar_solicitud 1,2069212,201413,'Edder','Delgado','Calderon',894912,'estudiante','edder@gmail.com',57.12,40.12,'si','200-01-013',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,2069213,201414,'Dylan','Blanco','Moraga',894913,'asistente','dylan@gmail.com',40.23,38.12,'si','200-01-014',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 2,2069214,201415,'Natalia','Cruz','Cordero',894914,'tutor','natalia@gmail.com',54.02,85.32,'no','200-01-015',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 2,2069215,201416,'Maria','Arias','Gomez',894915,'especial','maria@gmail.com',55.123,62.01,'si','200-01-016',NULL,NULL,NULL,'guardada','noRecibida','no',0,NULL,NULL
+execute insertar_solicitud 1,2069216,201417,'Valeria','Lacayo','Cordero',894916,'estudiante','valeria@gmail.com',70.252,70.789,'no','200-01-017',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,2069217,201418,'Sofia de los Angeles','Cortes','Nuñez',894917,'asistente','sofia@gmail.com',70.95,67.98,'si','200-01-018',NULL,NULL,NULL,'guardada','noRecibida','no',0,NULL,NULL
+execute insertar_solicitud 1,2069218,201419,'Hector','Sancho','Rojas',894918,'tutor','hector@gmail.com',70.13,64.156,'no','200-01-019',NULL,NULL,NULL,'guardada','noRecibida','no',0,NULL,NULL
+execute insertar_solicitud 1,2069219,201420,'Alejandra Fernanda','Rodriguez','Fernandez',894919,'especial','alejandrafernanda@gmail.com',67.23,65.12,'si','200-01-020',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206920,20141,'Anderson','Taylor','Cordero',89490,'estudiante','taylor@gmail.com',55.12,65,'si','200-01-001',NULL,NULL,NULL,'guardada','noRecibida','no',0,NULL,NULL
+execute insertar_solicitud 1,206920,20141,'Anderson','Taylor','Cordero',89490,'estudiante','taylor@gmail.com',55.12,65,'si','200-01-001',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206920,20141,'Anderson','Taylor','Cordero',89490,'estudiante','taylor@gmail.com',55.12,65,'si','200-01-001',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206920,20141,'Anderson','Taylor','Cordero',89490,'estudiante','taylor@gmail.com',55.12,65,'si','200-01-001',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206920,20141,'Anderson','Taylor','Cordero',89490,'estudiante','taylor@gmail.com',55.12,65,'si','200-01-001',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206920,20141,'Anderson','Taylor','Cordero',89490,'estudiante','taylor@gmail.com',55.12,65,'si','200-01-001',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+execute insertar_solicitud 1,206920,20141,'Anderson','Taylor','Cordero',89490,'estudiante','taylor@gmail.com',55.12,65,'si','200-01-001',NULL,NULL,NULL,'guardada','noRecibida','no',0,NULL,NULL
+execute insertar_solicitud 1,206920,20141,'Anderson','Taylor','Cordero',89490,'estudiante','taylor@gmail.com',55.12,65,'si','200-01-001',NULL,NULL,NULL,'enviada','pendiente','no',0,NULL,NULL
+
+execute insertar_solicitud_especial 6,10
+execute insertar_solicitud_especial 10,10
+execute insertar_solicitud_especial 14,20
+execute insertar_solicitud_especial 18,20
+execute insertar_solicitud_especial 22,10
+
+execute insertar_solicitud_ta 4,'CE1212','Introduccion',85,'Marco Rivera',NULL
+execute insertar_solicitud_ta 5,'CE1545','Digitales',80,'Nereo Campos',NULL
+execute insertar_solicitud_ta 8,'CE1545','Digitales',80,'Nereo Campos',NULL
+execute insertar_solicitud_ta 9,'CE1545','Digitales',80,'Nereo Campos',NULL
+execute insertar_solicitud_ta 12,'CE1212','Introduccion',85,'Marco Rivera',NULL
+execute insertar_solicitud_ta 13,'CE1545','Digitales',80,'Nereo Campos',NULL
+execute insertar_solicitud_ta 16,'CE1212','Introduccion',85,'Marco Rivera',NULL
+execute insertar_solicitud_ta 17,'CE1545','Digitales',80,'Nereo Campos',NULL
+execute insertar_solicitud_ta 20,'CE1212','Introduccion',85,'Marco Rivera',NULL
+execute insertar_solicitud_ta 21,'CE1212','Introduccion',85,'Marco Rivera',NULL
+
+execute insertar_solicitud_certificacion 20141,1,2018,'pendiente'
+execute insertar_solicitud_certificacion 20142,1,2018,'pendiente'
+execute insertar_solicitud_certificacion 20143,1,2018,'pendiente'
+
+execute insertar_certificacion 1,20141,'Anderson','Taylor','Cordero',1,2018,50,'Marco Rivera'
+
+execute insertar_solicitud_cancelacion 1,'Taylor no puede con todo porque es muy meco'
+execute insertar_solicitud_cancelacion 7,'Geovanny no puede con todo porque es muy meco'
+execute insertar_solicitud_cancelacion 5,'Emilita si puede con todo porque no es muy meca'
+execute insertar_solicitud_cancelacion 10,'Baby Jairo no puede con todo porque es muy meco'
+execute insertar_solicitud_cancelacion 9,'Taylor no puede con todo porque es muy meco'
+
+execute cancelar 1
+execute cancelar 7
+execute cancelar 5
+
+execute avalar 2
+execute avalar 3
+execute avalar 4
+execute avalar 5
+execute avalar 6
+
+execute no_avalar 9,'Le falto un dato ahi'
+execute no_avalar 10,'El promedio esta diferente'
+execute no_avalar 11,'El screen de la nota no se ve'
+
+execute aprobar 12,50,'Marco Rivera'
+execute aprobar 13,50,'Marco Rivera'
+execute aprobar 14,50,'Nereo Campos'
+execute aprobar 15,50,'Marco Rivera'
+execute aprobar 16,50,'Nereo Campos'
+
+execute rechazar 22,'No cumple con requerimientos'
+execute rechazar 23,'Ponderado muy bajo'
+execute rechazar 24,'No tiene la nota requerida'
+
+
+execute asignar_extras 201412,15
+execute asignar_extras 201414,5
+execute asignar_extras 201416,30
+
+execute evaluar 201412,'no trabaja bien y siempre llega tarde','Marco Rivera'
+execute evaluar 201414,'excelente y muy ordenado','Nereo Campos'
+execute evaluar 201416,'super autodidacta','Nereo Campos'
+
+--execute ingresar_periodo '10/10/2018','11/10/2018'
+
+--execute get_carnet_nombre 5
